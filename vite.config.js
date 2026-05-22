67d3fd1d9ce8c923747c35af904108d7c436a573
vikat.gen <vikat.gen@gmail.com>
2026-05-22 09:57:46 +0300

    added eslint, prettier for formatting beatiful code

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import eslint from 'vite-plugin-eslint'
+
 
 export default defineConfig({
     plugins: [
         tailwindcss(),
-    ],
+        eslint()
+    ]
 })
