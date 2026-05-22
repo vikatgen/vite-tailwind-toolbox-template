018f5a121faaa4160dc22fd1c7102ccd7f9b4b5d
vikat.gen <vikat.gen@gmail.com>
2026-05-22 09:59:40 +0300

    prettier code format

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-import eslint from 'vite-plugin-eslint'
-
+import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
+import eslint from 'vite-plugin-eslint';
 
 export default defineConfig({
-    plugins: [
-        tailwindcss(),
-        eslint()
-    ]
-})
+    plugins: [tailwindcss(), eslint()],
+});
